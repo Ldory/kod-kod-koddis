34ea9f149ae23652d3b9622e7df48a6baefbd491
Ldory <40350674+Ldory@users.noreply.github.com>
2018-07-18 13:22:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!'
+const prefix = '*'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -27,5 +27,95 @@ client.user.setGame(`KODESIGN.`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+client.on('message', msg => {
+  if (msg.content === 'السلام عليكم') {
+    msg.reply('**وعليكم السلام ورحمه الله وبركاته**');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'برب') {
+    msg.reply('**تيت**');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'باك') {
+    msg.reply('**ولكم منور ي حب**');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'الو') {
+    msg.reply('**هلا ؟**');
+  }
+});
+  client.on('message',async message => {
+  let mention = message.mentions.members.first();
+  let role = message.content.split(" ").slice(2).join(" ");
+  let mySupport = message.guild.roles.find('name',role);
+  let acRoom = client.channels.get('Channel ID');
+  if(message.content.startsWith(prefix + "قبول")) {
+    if(message.guild.id !== 'Server ID') return;
+    if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
+    if(!mention) return message.reply('منشن شخص');
+    if(!role) return message.reply('ادخل اسم رتبة');
+    if(!mySupport) return message.reply('هذه الرتبة غير موجودة');
+    if(mention.roles.has(mySupport)) return message.reply('هذا الشخص معه الرتبة مسبقا');
 
+    mention.addRole(mySupport).then(() => {
+      acRoom.send(`**[ ${mySupport} ] واعطائك رتبة ${mention} تم بنجاح قبولك**`);
+    });
+  }
+});
+  client.on('message',async message => {
+  let mention = message.mentions.members.first();
+  let acRoom = client.channels.get('ROOM ID');
+  if(message.content.startsWith(prefix + "رفض")) {
+  if(message.guild.id !== 'SERVER ID') return;
+  if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return;
+  if(!mention) return message.reply("منشن شخص");
+
+  acRoom.send(`**${mention} تم رفضك للاسف**`)
+  }
+});
+  client.on('message', async message => {
+  if(message.content.startsWith(prefix + "تقديم")) {
+    await message.channel.send("**:writing_hand: ارسل اللغة البرمجة الآن **").then(e => {
+    let filter = m => m.author.id === message.author.id
+    let lan = '';
+    let md = '';
+    let br = '';
+    let chaLan = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+    .then(collected => {
+      lan = collected.first().content
+      collected.first().delete()
+e.edit(`**:ارسل مدة خبرتك الان**`)
+let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+.then(co => {
+  md = co.first().content
+        co.first().delete()
+        e.edit(`**ارسل ماذا تستطيع ان تنشأ تصاميم **`)
+let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+.then(col => {
+  br = col.first().content
+        col.first().delete()
+e.edit("**جاري التقديم علي طلبك...**").then(b => {
+        setTimeout(() => {
+  b.edit(`**تم التقديم وسيتم الرد فـ اقرب وقت**`)
+        },2000);
+var gg = message.guild.channels.find('name', 'التقديمات')
+if(!gg) return;
+if(gg) {
+gg.send({embed : new Discord.RichEmbed()
+.setDescription(`**وش تبيع : \n ${lan}\n مدة تعلم التصميم:\n ${md} \nالخبرة  :\n ${br}  **`)  
+          .setFooter(`Codes.`)
+.setTimestamp()
+});
+}        
+})
+})
+})
+})
+})
+ }
+    
+    
 client.login(process.env.BOT_TOKEN);
