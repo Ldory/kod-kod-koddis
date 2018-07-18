7e57ba1779f6fee6952c962a56c183356eaccb04
Ldory <40350674+Ldory@users.noreply.github.com>
2018-07-18 14:47:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,9 +90,9 @@ client.on('message', msg => {
   }
 });
     client.on('message', message => {
-         var prefix = "-"
+         var prefix = "*"
         if(!message.channel.guild) return;
-     if(message.content.startsWith(prefix + 'bc')) {
+     if(message.content.startsWith(prefix + '2bc')) {
      if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
      if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
      let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
